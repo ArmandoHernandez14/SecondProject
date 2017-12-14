09ffb6b6a7e0d0812d9131ac0703e9a27031db2e
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2017-12-13 23:31:30 -0800

    C++

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//cpp boiler
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+#include <sstream>
+#include <limits>
+#include <cmath>
+//#include "node.h"
+//---------------- CURENT TASK ---------------
+//--------------------------------------------
+using namespace std;
+
+int main()
+{
+std::cout << "Hello Dr.Hernandez!!!" << '\n';
+const double PI =3.1315926535;
+char myGrade ='A';
+book isHappy = true;
+int myAge = 39;
+float favNum =3.1315926535;
+double otherfavNum =1.6180339887;
+
+cout <<"Favorite Number" << favNum << '\n';
+return 0;
+}
