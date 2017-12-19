071ad20f8b4b27017f134132004d5fc1ea19bed7
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2017-12-18 18:09:23 -0800

    sum

---
+++ Exo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+int main(){
+  cout << "Hello World" <<endl;
+
+  const double PI =3.1315926535;
+  char myGrade ='A';
+  book isHappy = true;
+  int myAge = 39;
+  float favNum =3.1315926535;
+  double otherfavNum =1.6180339887;
+
+  cout <<"Favorite Number" << favNum << '\n';
+}
--- main.cpp
+++ main.cpp
@@ -13,14 +13,10 @@ using namespace std;
 
 int main()
 {
-std::cout << "Hello Dr.Hernandez!!!" << '\n';
-const double PI =3.1315926535;
-char myGrade ='A';
-book isHappy = true;
-int myAge = 39;
-float favNum =3.1315926535;
-double otherfavNum =1.6180339887;
+  int count=10;
+  for (size_t i = 0; i < count; i++) {
+  cout << "Hello Dr.Hernandez!!!" << '\n';
+  }
 
-cout <<"Favorite Number" << favNum << '\n';
 return 0;
 }
