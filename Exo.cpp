eeec321a78218731f92a3dbf61d4299268927f0e
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2017-12-19 18:53:22 -0800

    a

--- Exo.cpp
+++ Exo.cpp
@@ -10,10 +10,16 @@ int main(){
 
   const double PI =3.1315926535;
   char myGrade ='A';
-  book isHappy = true;
   int myAge = 39;
-  float favNum =3.1315926535;
+  float favNum = 3.1315926535;
   double otherfavNum =1.6180339887;
 
-  cout <<"Favorite Number" << favNum << '\n';
+  cout <<"Favorite Number" << otherfavNum <<endl;
+  cout<<"Size of int"<< sizeof(myAge)<< endl;
+  // Other types include
+  //Short int: At least 16 bits
+  // Long int: At least 32 bits
+  //Long Long int: At least 64 bits
+  //unsigned int: Same size as signed version
+  // long double:Not less then double
 }
---
+++ Makefile.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+  cout<<"Hello Dr.Hernandez"<<endl;
+  return 0;
+}
