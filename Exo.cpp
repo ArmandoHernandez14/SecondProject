5da110eca8aed01e605462628966b9cc64e45365
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2017-12-21 00:40:20 -0800

    xbox

--- Exo.cpp
+++ Exo.cpp
@@ -1,17 +1,24 @@
 #include<iostream>
 
-
 using namespace std;
 
-int main(){
-  string a;
-  cout<<"What is your name? \n";
-  cin>>a;
-  cout<< "Hello "<<a<<endl;
-  // Other types include
-  //Short int: At least 16 bits
-  // Long int: At least 32 bits
-  //Long Long int: At least 64 bits
-  //unsigned int: Same size as signed version
-  // long double:Not less then double
+void Hernandez(int x){
+  cout<<"Dr.Hernandez's favorite number is "<< x <<endl;
 }
+
+int main() {
+  Hernandez(14);
+  return 0;
+}
+// int main(){
+//   string a;
+//   cout<<"What is your name? \n";
+//   cin>>a;
+//   cout<< "Hello "<<a<<endl;
+//   // Other types include
+//   //Short int: At least 16 bits
+//   // Long int: At least 32 bits
+//   //Long Long int: At least 64 bits
+//   //unsigned int: Same size as signed version
+//   // long double:Not less then double
+// }
