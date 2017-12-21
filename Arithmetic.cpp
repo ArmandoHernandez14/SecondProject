e85f9ac4ad4c3eb263a39cc9fd61f37af6ad64e9
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2017-12-20 20:22:49 -0800

    Brett

---
+++ Arithmetic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+//  int x= (4+8)*9;/* The computer multiplies by the Associate Property. So if you want to have some numbers to get multiplyied or subtracted first, your gonna have to put them in parenthesis */
+//  cout<<x; (2==2); That's how you write equal to in C++
+// (2!=3); That's how you put not equal too
+if(8!=3){
+  cout<< "Armando is awesome!!!!";
+}
+
+
+
+  return 0;
+}
