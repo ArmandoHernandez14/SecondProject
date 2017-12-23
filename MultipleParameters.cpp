2f791c8c936667065bcd1c86e23b97dcaf823e39
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2017-12-22 20:17:49 -0800

    Gross Pay calculator

--- MultipleParameters.cpp
+++ MultipleParameters.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 using namespace std;
-
+// If you want to Multiply,Divide,Subtract,and Add you need to just add more integers in the ()
 int addNumbers(int x, int y){
    int answer = x + y;
    return answer;
 }
 
 int main(){
-  cout<< addNumbers(43, 86);
+  cout<< addNumbers(81, 86);
   return 0;
 }
