75263e8be3f845aabf20b37a40655cee019f3cbd
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2017-12-24 04:48:25 -0800

    C++

--- Assignment.cpp
+++ Assignment.cpp
@@ -2,13 +2,14 @@
 #include<string>
 
 using namespace std;
-
+char again ='Y';
 int main(){
 int mx=130;
 int n;
 int additional;
 int sorry;
-
+while (again == 'y' || again == 'Y') {
+  /* code */
 cout<<"How many people will be attending the meeting?\n";
 cin>>n;
 
@@ -20,6 +21,9 @@ else{
   sorry= n-mx;
   cout<<"The meeting cannot be held as planned due to fire regulations and in order to continue "<< sorry<<" people must leave."<<endl;
 }
-
+// cout <<"Go again? (y/n) " ; If you want to end loop, just type this
+// cin>> again; // change control variable
+}//end while loop
+// cout<<"The end"<<endl;
   return 0;
 }
