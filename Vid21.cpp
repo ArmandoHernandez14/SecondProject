8893c832cc4e599825e09593b6923c4e704f9d60
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2017-12-25 01:56:17 -0800

    p

---
+++ Vid21.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+ int  x=10;
+// x -=5;// += is x + 5, -= is x-5, *= is x*5, and /= is x/5, also %= which is mojules
+
+ cout<<x++<< endl;
+ cout<<x<<endl;
+
+  return 0;
+}
---
+++ forLoop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  for (int x = 5; x<=50; x+=5) {
+    /* code */ cout<<x<<endl;
+  }
+
+
+}
