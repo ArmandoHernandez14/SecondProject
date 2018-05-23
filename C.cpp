a6507f7ef6f9d0558400a02a123ab34b9f1668cc
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2018-05-22 18:59:37 -0700

    Assignment 2

--- C.cpp
+++ C.cpp
@@ -1,16 +1,44 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
-int main() {
-  char symbol1, symbol2, symbol3;
 
-  cout<<"Enter two initials, without any periods:\n";
-  cin>>symbol1>> symbol2;
-  cout<<"The two initials are:\n";
-  cout<<symbol1<<symbol2<< endl;
-  cout<<"Once more with a space:\n";
-  symbol3 = ' ';
-  cout<< symbol1 << symbol3 << symbol2 << endl;
-  cout << "That's all.";
-  return 0;
+void Input(double& s1,double& s2,double &s3, double & s4);
+//Takes the four scores from the user
+void Compute(double s1,double s2,double s3, double s4,double& Av, double &Std_devia);
+//Calculates the Average, and Standard Deviation of the Four Scores
+void Output(double Av, double Std_devia);
+//Displays the results to the screen
+
+int main(){
+   double s1,s2,s3,s4,Av,Std_devia;
+   int end;
+   do {
+     Input(s1,s2,s3,s4);
+     Compute(s1,s2,s3,s4,Av,Std_devia);
+     Output(Av,Std_devia);
+     cout<<"Enter -1 to end: ";
+     cin>>end;
+   } while(end != -1);
+}
+
+void Input(double& s1,double& s2,double &s3, double & s4){
+  cout<<"Enter the First score: ";//Takes the first score
+  cin>>s1;
+  cout<<"Enter the Second score: ";//Takes the Second Score
+  cin>>s2;
+  cout<<"Enter the Third score: ";//Takes the Third score
+  cin>>s3;
+  cout<<"Enter the Fourth score: ";//Takes the fourth score
+  cin>>s4;
+}
+
+void Compute(double s1,double s2,double s3, double s4,double& Av, double& Std_devia){
+  Av=((s1+s2+s3+s4)/4);
+  Std_devia= sqrt((s1-Av)*(s1-Av));
+}
+
+void Output(double Av, double Std_devia){
+  cout<<"The Average score(of those four scores) is "<<Av<<"% , and the Standard Deviation is ";
+  cout<<Std_devia<<"%.\n";
 }
